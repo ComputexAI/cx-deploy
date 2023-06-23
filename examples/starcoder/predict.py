b9c8613ad25b4a515d475c95d3fad078aa26b39b
Abate De Mey <abatedemey@gmail.com>
2023-06-22 23:50:34 -0700

    Add Starcoder Bigcode example

--- examples/starcoder/predict.py
+++ examples/starcoder/predict.py
@@ -1,25 +1,56 @@
 from cog import BasePredictor, Input
 from typing import Any
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-
-MODEL_PATH = "./files"
 DEVICE = "cuda"
+
+
+def get_token():
+    token = ""
+    with open("/src/huggingface_token", "r") as f:
+        token = f.read()
+    return token
+
+
+TOKEN = get_token()
 
 
 class Predictor(BasePredictor):
     def setup(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-        self.model = AutoModelForCausalLM.from_pretrained(MODEL_PATH).to(DEVICE)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "bigcode/starcoder", use_auth_token=TOKEN
+        )
+
+        # Load the model in 16 bit precision
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "bigcode/starcoder",
+            use_auth_token=TOKEN,
+            device_map="auto",
+            torch_dtype=torch.float16,
+        )
+
+        # Load the model in 32 bit precision
+        # self.model = AutoModelForCausalLM.from_pretrained(
+        #     "bigcode/starcoder", use_auth_token=TOKEN, device_map="auto"
+        # )
+
+        print(f"Memory footprint: {self.model.get_memory_footprint() / 1e6:.2f} MB")
 
     def predict(
         self,
         prompt: str = Input(description="Code prompt"),
-        max_length: int = Input(default=100, description="Maximum length of generated code"),
+        max_length: int = Input(
+            default=100, description="Maximum length of generated code"
+        ),
         top_p: float = Input(default=0.95, description="Top p value for sampling"),
         top_k: int = Input(default=50, description="Top k value for sampling"),
-        temperature: float = Input(default=0.2, description="Temperature value for sampling"),
-        num_return_sequences: int = Input(default=1, description="Number of sequences to return"),
+        temperature: float = Input(
+            default=0.2, description="Temperature value for sampling"
+        ),
+        num_return_sequences: int = Input(
+            default=1, description="Number of sequences to return"
+        ),
     ) -> Any:
         """Run a single prediction on the model"""
 
@@ -35,4 +66,9 @@ class Predictor(BasePredictor):
             num_return_sequences=num_return_sequences,
         )
 
-        return result
+        # Decode the generated output
+        generated_code = self.tokenizer.decode(
+            result[0], skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+
+        return generated_code
